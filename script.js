a625115c7aa2ea31c1c83558c07a9878c0468960
leahtorres <leah_torres@davincischools.org>
2017-11-30 14:14:34 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// This function is run when the user clicks the image on LINE 13 of your HTML (Gohan on the cloud)
+function flyGohan(e){
+	document.getElementById("nimbus").classList.add("fly-forward");
+}
+
+// This function is run when the user clicks on the image on LINE 18 of your HTML (Vegeta powering up)
+function changePicture(e){
+	document.getElementById("vegeta").classList.toggle("power-up");
+}
